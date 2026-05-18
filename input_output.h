de1a9743e5a95faca2baff062a6aed346a0f1578
Galkin <galkin.k2007@gmail.com>
2026-05-18 23:53:40 +0700

    Добавлены основные функции упрощающие ввод информации для последующей обработки, также добавлены функции для удобного вывода.

--- input_output.h
+++ input_output.h
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 #include <vector>
 #include <cstdint>
 #include <string>
 
 
-std::vector<uint32_t> transformStream(std::istream& source);
+std::vector<uint8_t> fromStreamToData(std::istream& source);
 std::string dataToHex(const std::vector<uint8_t>& data);
+std::vector<uint8_t> readConsoleToBytes();
 bool dataToBinaryFile(const std::vector<uint8_t>& data, const std::string& filename);
