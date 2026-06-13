80c4adbb30786cfccd620b22d6ebebb4675c249b
Lik37 <starodubtsevivan917@gmail.com>
2026-06-13 21:08:18 +0700

    Убраны значения по умолчанию у функций в шифрах

--- ciphers/rc4.h
+++ ciphers/rc4.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 
 // Генерация случайного ключа заданной длины
-std::vector<uint8_t> generateRC4Key(size_t length = 255);
+std::vector<uint8_t> generateRC4Key(size_t length);
 
 // Трансформация вектора байт, шифрование и расшифрование
 std::vector<uint8_t> rc4Transform(const std::vector<uint8_t>& data, const std::vector<uint8_t>& key);
--- ciphers/rc5.h
+++ ciphers/rc5.h
@@ -3,10 +3,10 @@
 #include <cstdint>
 //w=32
 // Генерация случайного ключа заданной длины (по умолчанию 16 байт)
-std::vector<uint8_t> generateRC5Key(size_t length = 16);
+std::vector<uint8_t> generateRC5Key(size_t length);
 
 // Шифрование (паддинг PKCS7)
-std::vector<uint8_t> rc5Encrypt(const std::vector<uint8_t>& data, const std::vector<uint8_t>& key, unsigned int rounds = 12);
+std::vector<uint8_t> rc5Encrypt(const std::vector<uint8_t>& data, const std::vector<uint8_t>& key, unsigned int rounds);
 
 // Дешифрование (снятие паддинга PKCS7)
-std::vector<uint8_t> rc5Decrypt(const std::vector<uint8_t>& data, const std::vector<uint8_t>& key, unsigned int rounds = 12);
+std::vector<uint8_t> rc5Decrypt(const std::vector<uint8_t>& data, const std::vector<uint8_t>& key, unsigned int rounds);
